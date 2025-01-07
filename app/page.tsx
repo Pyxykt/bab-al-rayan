7dc4ebc8063dfcf7d1ecdf48806f62496eac199d
Tipu <tipualikhan@gmail.com>
2025-01-07 22:02:36 +0530

    Initialized Bab-al-rayan

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 min-h-lvh font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-9xl text-orange-600">Bab-al-rayan</h1>
+      <p className="text-8xl text-amber-500">Coming soon</p>
+    </div>
+  );
+}
